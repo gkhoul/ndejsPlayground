4dc940e933e2508f88de72a3e96ec0e29621547f
gkhoul <gkhoul@yandex.ru>
2016-07-12 16:44:08 +0500

    Installed jake + created default task

---
+++ Jakefile.js
@@ -0,0 +1,15 @@
+/**
+ * Created by usr on 12.07.2016.
+ */
+/* global console */
+
+(function () {
+    'use strict';
+
+    task("default", function () {
+        console.log("default task");
+
+    });
+
+    console.log("\nBuild Ok");
+}());
