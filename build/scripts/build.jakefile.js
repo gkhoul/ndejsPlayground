f384db5855640dc6f60bc67f635de05b92716ff1
gkhoul <gkhoul@yandex.ru>
2016-07-12 21:02:24 +0500

    Installed object-merge

--- build/scripts/build.jakefile.js
+++ build/scripts/build.jakefile.js
@@ -1,9 +1,11 @@
+/* global desc, task, console, require, complete, fail, process */
+
 (function () {
     'use strict';
 
     var startTime = Date.now();
 
-    desc("Lint and test")
+    desc("Lint and test");
     task("default", [ "version", "lint" ], function () {
         var elapsedSeconds = (Date.now() - startTime) / 1000;
         console.log("\nBuild Ok + (" + elapsedSeconds.toFixed(2) + "s)");
@@ -11,8 +13,17 @@
 
     desc("Linting");
     task("lint", function () {
-        console.log("Linting: .");
-    });
+        var jshint = require("simplebuild-jshint");
+        var jshintConfig = require("../config/jshint.conf.js");
+        process.stdout.write("Linting: .");
+        jshint.checkFiles({
+            files: [ "build/**/*.js" ],
+            options: {
+                node: jshintConfig.clientOptions,
+            },
+            globals: jshintConfig.nodeGlobals
+        }, complete, fail );
+    }, {async: true});
 
     desc("Check version");
     task("version", function () {
