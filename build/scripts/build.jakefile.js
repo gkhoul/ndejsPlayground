c2d1ff316e63950d20e7c79dfa1377a8e3344eb2
gkhoul <gkhoul@yandex.ru>
2016-07-12 20:44:33 +0500

    Installed jshint and jshint-simplebuild

--- build/scripts/build.jakefile.js
+++ build/scripts/build.jakefile.js
@@ -3,11 +3,18 @@
 
     var startTime = Date.now();
 
-    task("default", [ "version" ], function () {
+    desc("Lint and test")
+    task("default", [ "version", "lint" ], function () {
         var elapsedSeconds = (Date.now() - startTime) / 1000;
         console.log("\nBuild Ok + (" + elapsedSeconds.toFixed(2) + "s)");
     });
 
+    desc("Linting");
+    task("lint", function () {
+        console.log("Linting: .");
+    });
+
+    desc("Check version");
     task("version", function () {
         console.log("Checking node version: .");
         var version = require("./../util/version_checker.js");
