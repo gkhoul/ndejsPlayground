fc93a7d467fef01c4c5993312bae378408b0af49
gkhoul <gkhoul@yandex.ru>
2016-07-14 16:51:48 +0500

    Continue testing

--- src/client/example.js
+++ src/client/example.js
@@ -2,18 +2,18 @@
 
 /*global require, exports*/
 
-(function() {
-	"use strict";
+(function () {
+    "use strict";
 
-	var constants = require("./constants.js");
+    var constants = require("./constants.js");
 
-	exports.validateTextField = function validateTextField(field) {
-		if (field.value) {
-			field.removeAttribute("class");
-		}
-		else {
-			field.setAttribute("class", constants.REQUIRED_FIELD_CLASS);
-		}
-	};
+    exports.validateTextField = function validateTextField(field) {
+        if (field.value) {
+            field.removeAttribute("class");
+        }
+        else {
+            field.setAttribute("class", constants.REQUIRED_FIELD_CLASS);
+        }
+    };
 
 }());
--- src/test.js
+++ src/test.js
@@ -1,6 +1,5 @@
-/**
- * Created by usr on 13.07.2016.
- */
+/*global describe, it, require */
+
 (function () {
     'use strict';
 
@@ -11,12 +10,12 @@
         it("adds numbers", function () {
             assert.equal(add(4, 5), 9);
         });
+
         it("uses IEEE 754 floating point", function () {
             assert.equal(add(0.1, 0.2), 0.30000000000000004);
-        })
+        });
 
     });
-
 
     function add(a, b) {
         return a + b;
