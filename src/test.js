83ff3f845a4bf6caf54ecf137a493e40334c7b39
gkhoul <gkhoul@yandex.ru>
2016-07-14 10:29:24 +0500

    added simple test for mocha

--- src/test.js
+++ src/test.js
@@ -4,5 +4,22 @@
 (function () {
     'use strict';
 
+    var assert = require("chai").assert;
+
+    describe("Addition", function () {
+
+        it("adds numbers", function () {
+            assert.equal(add(4, 5), 9);
+        });
+        it("uses IEEE 754 floating point", function () {
+            assert.equal(add(0.1, 0.2), 0.30000000000000004);
+        })
+
+    });
+
+
+    function add(a, b) {
+        return a + b;
+    }
 
 }());
