dea7c2198d74f37c48e18980ee3343a3152fb990
gkhoul <gkhoul@yandex.ru>
2016-07-16 22:29:08 +0500

    Exports

--- src/client/test.js
+++ src/client/test.js
@@ -5,24 +5,23 @@
 
     // var assert = require("chai").assert;
 
+    var addition = require("./addition.js");
+
     describe("Addition", function () {
 
         it("adds numbers", function () {
-            assertEqual(add(4, 5), 9);
+            assertEqual(addition.add(4, 5), 9);
         });
 
         it("uses IEEE 754 floating point", function () {
-            assertEqual(add(0.1, 0.2), 0.30000000000000004);
+            assertEqual(addition.add(0.1, 0.2), 0.30000000000000004);
         });
 
     });
 
     function assertEqual(actual, expected) {
-        if (actual != expected) throw new Error("Expected " + expected + ", but got" + actual);
+        if (actual != expected) throw new Error("Expected " + expected + ", but got " + actual);
     }
 
-    function add(a, b) {
-        return a + b;
-    }
 
 }());
